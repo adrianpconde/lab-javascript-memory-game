61f983ca507f30b88530b208be09cf737e5b70bf
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-05-24 17:59:33 +0200

    Iteration 2.2 Class Methods done

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,42 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    this.pickedCards = []:
+    this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+
+    const randomCards = []
+    for (let i = 0; i < this.cards.length; i++) {
+      const indexCard = Math.floor(Math.random() * this.cards.length);
+      randomCards.push(indexCard[i]);
+    }
+
+    return randomCards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsClicked += 1;
+      this.pairsGuessed +=1;
+      return true;
+    } else {
+      this.pairsClicked += 1;
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
