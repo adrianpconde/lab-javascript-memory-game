85eebf1aa8f31eec6173875584a599cefa30f4ed
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-05-24 18:23:04 +0200

    Iterations 1 & 2 completed

--- js/memory.js
+++ js/memory.js
@@ -11,25 +11,22 @@ class MemoryGame {
       return undefined;
     }
 
-    const randomCards = []
-    for (let i = 0; i < this.cards.length; i++) {
-      const indexCard = Math.floor(Math.random() * this.cards.length);
-      randomCards.push(indexCard[i]);
-    }
-
-    return randomCards;
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let randomCards = Math.floor(Math.random() * (i + 1));  
+      return [this.cards[i], this.cards[randomCards]] = [this.cards[randomCards], this.cards[i]];
   }
+}
 
   checkIfPair(card1, card2) {
     if (card1 === card2) {
       this.pairsClicked += 1;
-      this.pairsGuessed +=1;
+      this.pairsGuessed += 1;
       return true;
     } else {
       this.pairsClicked += 1;
       return false;
     }
-  }
+  };
 
   checkIfFinished() {
     if (this.pairsGuessed === this.cards.length / 2) {
